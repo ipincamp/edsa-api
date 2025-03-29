22fafe925869edd16b1d59f9a2ee015b3bb8c4c6
ipincamp <support@nur-arifin.my.id>
2025-03-29 13:04:46 +0700

    feat: Update API routes to include authentication endpoints for login, registration, and logout

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('/login', 'signIn')->name('auth.signIn');
+    Route::post('/register', 'signUp')->name('auth.signUp');
+    Route::post('/logout', 'signOut')->name('auth.signOut')->middleware('auth:sanctum');
+});
