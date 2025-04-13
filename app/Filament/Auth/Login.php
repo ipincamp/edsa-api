0740561a398f0ce9a3a89fa1646b76902c1602b5
ipincamp <support@nur-arifin.my.id>
2025-04-13 10:30:53 +0700

    feat: Implement custom login form with username and password handling

---
+++ app/Filament/Auth/Login.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Auth;
+
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
+use Filament\Http\Responses\Auth\LoginResponse;
+use Filament\Pages\Auth\Login as BaseAuth;
+use Illuminate\Validation\ValidationException;
+
+class Login extends BaseAuth
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getUsernameFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getUsernameFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label('Email')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        return [
+            $login_type => $data['login'],
+            'password'  => $data['password'],
+        ];
+    }
+
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            return parent::authenticate();
+        } catch (ValidationException) {
+            throw ValidationException::withMessages([
+                'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+            ]);
+        }
+    }
+}
