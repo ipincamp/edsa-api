bbc36fdb1b680b0e07037fa0470361f17d4b1eaf
ipincamp <support@nur-arifin.my.id>
2025-05-11 23:33:58 +0700

    feat: Enhance Group management by adding course association and refining participant handling

--- app/Filament/Resources/GroupResource.php
+++ app/Filament/Resources/GroupResource.php
@@ -5,8 +5,6 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\GroupResource\Pages;
 use App\Filament\Resources\GroupResource\RelationManagers;
 use App\Models\Group;
-use App\Models\Student;
-use App\Models\Teacher;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -21,97 +19,72 @@ class GroupResource extends Resource
 
     protected static ?string $navigationGroup = 'Managements';
     protected static ?string $navigationLabel = 'Groups';
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 2;
     protected static ?string $label = 'Group';
-    protected static ?string $pluralLabel = 'Group of Class';
-    protected static ?string $slug = 'groups';
+    protected static ?string $pluralLabel = 'Data Groups';
 
     public static function form(Form $form): Form
     {
-        $columns = [
-            'default' => 1,
-            'sm' => 2,
-            'md' => 3,
-        ];
-
         return $form
             ->schema([
-                Forms\Components\Wizard::make()
-                    ->steps([
-                        Forms\Components\Wizard\Step::make('Details')
-                            ->schema([
-                                Forms\Components\TextInput::make('name')
-                                    ->columnSpanFull()
-                                    ->columns($columns)
-                                    ->required()
-                                    ->label('Name')
-                                    ->maxLength(255)
-                                    ->unique(ignoreRecord: true),
-                                Forms\Components\Textarea::make('description')
-                                    ->columnSpanFull()
-                                    ->columns($columns)
-                                    ->label('Description')
-                                    ->maxLength(255),
-                            ]),
-                        Forms\Components\Wizard\Step::make('Users')
-                            ->schema([
-                                Forms\Components\CheckboxList::make('teachers')
-                                    ->columns($columns)
-                                    ->relationship('teachers', 'name')
-                                    ->required()
-                                    ->label('Teachers')
-                                    ->bulkToggleable()
-                                    ->afterStateUpdated(function (callable $set, $state, $record) {
-                                        $set('teachers', $state);
+                // group
+                Forms\Components\Fieldset::make()
+                    ->label('Group Information')
+                    ->schema([
+                        // name
+                        Forms\Components\TextInput::make('name')
+                            ->columnSpanFull()
+                            ->label('Name')
+                            ->required()
+                            ->maxLength(50),
 
-                                        if ($record) {
-                                            activity('group')
-                                                ->performedOn($record)
-                                                ->event('updated')
-                                                ->withProperties([
-                                                    'attributes' => [
-                                                        'name' => $record->name,
-                                                        'teachers' => Teacher::whereIn('id', $state)->pluck('name')->toArray(),
-                                                    ],
-                                                    'old' => [
-                                                        'name' => $record->name,
-                                                        'teachers' => $record->teachers->pluck('name')->toArray(),
-                                                    ],
-                                                ])
-                                                ->log('Updated teacher in group');
-                                        }
-                                    }),
-                                Forms\Components\CheckboxList::make('students')
-                                    ->columns($columns)
-                                    ->relationship('students', 'name')
-                                    ->required()
-                                    ->label('Students')
-                                    ->bulkToggleable()
-                                    ->afterStateUpdated(function (callable $set, $state, $record) {
-                                        $set('students', $state);
+                        // description
+                        Forms\Components\Textarea::make('description')
+                            ->columnSpanFull()
+                            ->label('Description')
+                            ->rows(2)
+                            ->maxLength(255),
 
-                                        if ($record) {
-                                            activity('group')
-                                                ->performedOn($record)
-                                                ->event('updated')
-                                                ->withProperties([
-                                                    'attributes' => [
-                                                        'name' => $record->name,
-                                                        'students' => Student::whereIn('id', $state)->pluck('name')->toArray(),
-                                                    ],
-                                                    'old' => [
-                                                        'name' => $record->name,
-                                                        'students' => $record->students->pluck('name')->toArray(),
-                                                    ],
-                                                ])
-                                                ->log('Updated students in group');
-                                        }
-                                    }),
-                            ]),
-                    ])
-                    ->columnSpanFull(),
+                        // course
+                        Forms\Components\Select::make('course_id')
+                            ->columnSpanFull()
+                            ->label('Course')
+                            ->options(\App\Models\Course::query()->pluck('name', 'id'))
+                            ->required(),
+                    ]),
+                // participants
+                Forms\Components\Fieldset::make()
+                    ->label('Participants')
+                    ->columnSpanFull()
+                    ->schema([
+                        // teachers
+                        Forms\Components\CheckboxList::make('teachers')
+                            ->label('Teachers')
+                            ->relationship('teachers', 'name')
+                            ->options(
+                                \App\Models\User::query()
+                                    ->whereHas('roles', function ($query) {
+                                        $query->whereIn('name', ['teacher']);
+                                    })
+                                    ->pluck('name', 'id')
+                            )
+                            ->columns(2)
+                            ->helperText('Select one or more teachers'),
 
-
+                        // students
+                        Forms\Components\CheckboxList::make('students')
+                            ->label('Students')
+                            ->relationship('students', 'name')
+                            ->options(
+                                \App\Models\User::query()
+                                    ->whereHas('roles', function ($query) {
+                                        $query->whereIn('name', ['student']);
+                                    })
+                                    ->pluck('name', 'id')
+                            )
+                            ->columns(2)
+                            ->helperText('Select one or more students'),
+                    ]),
             ]);
     }
 
@@ -120,30 +93,34 @@ class GroupResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
-                    ->label('Group Name')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('description')
-                    ->label('Description')
-                    ->limit(20)
+                    ->label('Group')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('participants')
+                Tables\Columns\TextColumn::make('participants_count')
                     ->label('Participants')
-                    ->getStateUsing(function ($record) {
-                        return $record->teachers()->count() + $record->students()->count();
-                    }),
+                    ->counts('participants'),
+            ])
+            ->groups([
+                Tables\Grouping\Group::make('course.name')
+                    ->titlePrefixedWithLabel(true),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
                 Tables\Actions\ActionGroup::make([
-                    Tables\Actions\ViewAction::make(),
-                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make()
+                        ->color('success')
+                        ->label('View')
+                        ->icon('heroicon-o-eye'),
+                    Tables\Actions\EditAction::make()
+                        ->color('warning')
+                        ->label('Details')
+                        ->icon('heroicon-o-pencil')
+                        ->closeModalByClickingAway(false),
                     Tables\Actions\DeleteAction::make(),
                     Tables\Actions\ForceDeleteAction::make(),
                     Tables\Actions\RestoreAction::make(),
-                ]),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -164,6 +141,7 @@ class GroupResource extends Resource
     public static function getEloquentQuery(): Builder
     {
         return parent::getEloquentQuery()
+            ->with('course')
             ->withoutGlobalScopes([
                 SoftDeletingScope::class,
             ]);
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\RoleEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -61,5 +62,19 @@ class Group extends Model
             'group_id',
             'user_id'
         )->withTimestamps();
+    }
+
+    public function teachers()
+    {
+        return $this->participants()->whereHas('roles', function ($query) {
+            $query->where('name', RoleEnum::TEACHER->value);
+        });
+    }
+
+    public function students()
+    {
+        return $this->participants()->whereHas('roles', function ($query) {
+            $query->where('name', RoleEnum::STUDENT->value);
+        });
     }
 }
