24258e31ff1551a7214d13d0458910d71d193360
ipincamp <support@nur-arifin.my.id>
2025-05-25 16:13:56 +0700

    feat: Enhance SummaryWidget to display scores for eight books and calculate average score
    masih untuk semua siswa

--- app/Filament/Widgets/SummaryWidget.php
+++ app/Filament/Widgets/SummaryWidget.php
@@ -24,21 +24,43 @@ class SummaryWidget extends BaseWidget
     {
         return $table
             ->query(
-                // TODO: Add relation to summaries
-                \App\Models\User::query()->withCount('groups'),
+                \App\Models\User::query()
+                    ->whereHas('roles', fn($query) => $query->where('name', 'student'))
+                    ->with(['progress' => fn($query) => $query->orderBy('book_id')])
             )
             ->columns(
-                collect(range(1, 8))->map(
-                    fn($i) => Tables\Columns\TextColumn::make("b_$i")
-                )->prepend(
+                [
                     Tables\Columns\TextColumn::make('name')
                         ->label('Student Name')
-                        ->limit(20)
-                )->push(
-                    Tables\Columns\TextColumn::make('average')
-                        ->label('Average')
-                        ->formatStateUsing(fn($state) => number_format($state, 2))
-                )->toArray()
+                        ->limit(20),
+                    Tables\Columns\TextColumn::make('progress.0.score_correct')
+                        ->label('Book 1')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.1.score_correct')
+                        ->label('Book 2')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.2.score_correct')
+                        ->label('Book 3')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.3.score_correct')
+                        ->label('Book 4')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.4.score_correct')
+                        ->label('Book 5')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.5.score_correct')
+                        ->label('Book 6')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.6.score_correct')
+                        ->label('Book 7')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('progress.7.score_correct')
+                        ->label('Book 8')
+                        ->default(0),
+                    Tables\Columns\TextColumn::make('average_score')
+                        ->label('Avg')
+                        ->getStateUsing(fn($record) => number_format(collect($record->progress)->avg('score_correct') ?? 0, 2))
+                ]
             );
     }
 
