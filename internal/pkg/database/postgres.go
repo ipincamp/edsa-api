ae8d9f6a084614adb837e902d302e3f274550fa4
ipincamp <support@nur-arifin.my.id>
2025-10-25 05:43:49 +0700

    feat: enhance NewPostgresConnection to support dynamic logging levels based on environment

--- internal/pkg/database/postgres.go
+++ internal/pkg/database/postgres.go
@@ -8,9 +8,17 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func NewPostgresConnection(dsn string) *gorm.DB {
+func NewPostgresConnection(dsn string, env string) *gorm.DB {
+	// Tentukan level log berdasarkan environment
+	var logLevel logger.LogLevel
+	if env == "production" {
+		logLevel = logger.Silent // Jangan log SQL di production
+	} else {
+		logLevel = logger.Info // Log SQL di development, dll.
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info), // Log SQL
+		Logger: logger.Default.LogMode(logLevel), // Gunakan logLevel dinamis
 	})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
